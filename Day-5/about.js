eb8173852c0762b617f1c5a9205573122ca8e6af
Janani C R <113664337+Janani2708@users.noreply.github.com>
2023-07-13 19:20:13 +0530

    Add files via upload

---
+++ Day-5/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Glam Mart</h1>
+      <p>Welcome to Glam Mart, your one-stop destination for all your beauty needs!</p>
+      <p>At Glam Mart, we believe that beauty should be accessible to everyone. That's why we offer a wide range of high-quality makeup products, skincare essentials, and beauty tools.</p>
+      <p>Our mission is to empower individuals to express their unique beauty and style. Whether you're a makeup enthusiast or a beauty beginner, we have the products and resources to help you on your beauty journey.</p>
+      <p>Discover our carefully curated collection of cosmetics, skincare products, and beauty accessories. We source our products from top brands known for their quality and innovation, ensuring that you have access to the best in the industry.</p>
+      <p>Our team of beauty experts is dedicated to providing you with personalized recommendations and expert advice. We understand that everyone's beauty needs are unique, and we're here to help you find the perfect products that enhance your natural beauty.</p>
+      <p>Join us at Glam Mart and embark on a beauty experience like no other. Start exploring our wide range of products, get inspired by our beauty tutorials, and connect with a vibrant community of beauty enthusiasts.</p>
+      <p>Thank you for choosing Glam Mart. We're excited to be a part of your beauty journey!</p>
+    </div>
+  );
+}
+
+export default AboutPage;
