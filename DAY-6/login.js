ac83616c6532bdb64cae6929c00cbcf4bd4212be
Janani C R <113664337+Janani2708@users.noreply.github.com>
2023-07-13 19:23:07 +0530

    Add files via upload

---
+++ DAY-6/login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import './login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  
+  
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='whole' style={{backgroundImage:"url(/images/img1.avif)", backgroundSize:"cover"}}>
+      <br></br>
+      <br></br>
+      <div className='cen'>
+        <br></br>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <h2>Login Page</h2>
+            <h3>Email:</h3>
+            <input type="email" value={email} onChange={handleEmailChange} required />
+          </label>
+          <br /><br></br>
+          <label>
+            <h3>Password:</h3>
+            <input type="password" value={password} onChange={handlePasswordChange} required />
+          </label>
+          <br />
+          <br></br><br></br>
+          <h4>Don't have an account?<Link to='/sign'>    SignUp</Link> </h4>
+          <br></br>
+          <Link to="/home"><Button variant='contained' type="submit" >Login</Button></Link><br></br>
+        </form></div>
+    </div>
+
+  );
+};
+
+export default Login;
